4abe93f26c31e7128558f8fca2d7360bbeecc9b8
ali korabbaslu <ali.korabbaslu@gmail.com>
2018-06-15 23:31:14 +0300

    cleanup

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,9 @@
 import { combineReducers } from 'redux';
-
 import homeReducer from './homeReducer';
-// import layoutReducer from './layout';
-// import allProductsReducer from './allProducts';
-
 import {reducer as formReducer} from 'redux-form';
 
 const combineRed = combineReducers({
     homeReducer,
-    // allProductsReducer,
-    // layoutReducer,
     form: formReducer
 });
 
